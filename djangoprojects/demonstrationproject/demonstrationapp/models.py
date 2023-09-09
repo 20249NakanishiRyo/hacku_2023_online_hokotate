072a74c6404bbdc4817f1ae8f1b078243589bbe0
Imtomo <n.renka0516@gmail.com>
2023-09-09 18:29:34 +0900

    ちょっとmodelsとviewsいじった

--- djangoprojects/demonstrationproject/demonstrationapp/models.py
+++ djangoprojects/demonstrationproject/demonstrationapp/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from jsonfield import JSONField
 # Create your models here.
 class RateModel(models.Model):
-    data = JSONField()
+ date = models.DateField()
+ open = models.FloatField()
+ high = models.FloatField()
+ low = models.FloatField()
+ close = models.FloatField()
