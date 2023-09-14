6dda9f4bac816ab1a2a2c2c204ed052593db5625
root <root@Galleria>
2023-09-15 05:03:18 +0900

    500handler

--- demonstrationapp/urls.py
+++ demonstrationapp/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     path('get_chart/', views.get_chart, name='get_chart'),
     path('predict_chart/', views.predict_chart, name='predict_chart'),
     ]
+
+handler500 = views.my_customized_server_error
--- demonstrationapp/views.py
+++ demonstrationapp/views.py
@@ -158,3 +158,10 @@ def create_input_data(data, look_back):
     y = np.array(y_tmp).reshape(data_size, 1,  len(raw_data))
 
     return y, X
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
