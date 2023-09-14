911870db95c193a2f6ff3d7da1dd60a8a0b09acd
20249NakanishiRyo <20249@toba-cmt.ac.jp>
2023-09-14 23:16:29 +0900

    ファイル整理

--- chart_project/chart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
- 
-app_name = 'chart'
-urlpatterns = [
-    path('index/', views.IndexView.as_view(), name='index'),
-    path('get_chart/', views.get_chart, name='get_chart'),
-]
