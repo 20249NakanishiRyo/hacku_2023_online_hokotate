1d69d7d3cc1aca48a27d9a078ebff1c40be2257f
root <root@Galleria>
2023-09-15 04:43:15 +0900

    path fix

--- demonstrationproject/settings.py
+++ demonstrationproject/settings.py
@@ -185,7 +185,7 @@ if not DEBUG:
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'demonstrationapp/staticfiles')
     STATIC_URL = '/static/'
 
     STATICFILES_DIRS = (
