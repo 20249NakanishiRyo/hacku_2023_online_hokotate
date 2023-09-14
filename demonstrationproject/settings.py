8fad82e62317ddbc28245de08d603e1cb80bcd36
root <root@Galleria>
2023-09-15 05:26:25 +0900

    debugv2

--- demonstrationproject/settings.py
+++ demonstrationproject/settings.py
@@ -34,41 +34,6 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    # ログ出力フォーマットの設定
-    'formatters': {
-        'production': {
-            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
-                      '%(pathname)s:%(lineno)d %(message)s'
-        },
-    },
-    # ハンドラの設定
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/{}/app.log'.format(PROJECT_NAME),
-            'formatter': 'production',
-        },
-    },
-    # ロガーの設定
-    'loggers': {
-        # 自分で追加したアプリケーション全般のログを拾うロガー
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Django自身が出力するログ全般を拾うロガー
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    },
-}
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
